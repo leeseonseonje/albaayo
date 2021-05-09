78e06bb92cfe153d58c22f9f32c619632d5d5f55
leeseonseonje <leeseonje9323@gamail.com>
2021-05-09 22:06:49 +0900

    aws

--- src/test/java/albaAyo/albaayo/AlbaayoApplicationTests.java
+++ src/test/java/albaAyo/albaayo/AlbaayoApplicationTests.java
@@ -1,13 +1,13 @@
-package albaAyo.albaayo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AlbaayoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package albaAyo.albaayo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AlbaayoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
